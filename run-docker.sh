21b53f88ddaf1820dea29756333da69c09ed0cf7
Pavel Serikov <pavel.p.serikov@gmail.com>
2023-08-24 01:58:44 +0300

    Added toc folding

--- run-docker.sh
+++ run-docker.sh
@@ -4,9 +4,9 @@
 # See also : https://github.com/envygeeks/jekyll-docker#readme
 
 docker run --rm \
-  --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD:/srv/jekyll:Z" \
   --publish [::1]:4000:4000 \
   jekyll/jekyll \
   jekyll serve
 
-xdg-open http://localhost:4000/
+# xdg-open http://localhost:4000/
